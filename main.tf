15c01671954b1b18066dd1028c2ff3bcfed561d4
rushimujumale <rushimujumale297@gmail.com>
2023-10-24 01:36:06 +0530

    l1

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
-module "hosted_zone" {
-  source      = "./modules/hosted_zone"
+module "private_hosted_zone" {
+  source      = "./modules/private_hosted_zone"
   domain_name = var.domain_name
   comment     = var.comment
-  # vpc_id      = var.vpc_id
+  vpc_id      = var.vpc_id
   # is_private  = var.is_private
 }
 
 # output "zone_id" {
 #   value = module.hosted_zone.zone_id
 # }
+module "public_hosted_zone" {
+  source      = "./modules/pulic_hosted_zone"
+  domain_name = var.domain_name
+  comment     = var.comment
+  # vpc_id      = var.vpc_id
+  # is_private  = var.is_private
+}
